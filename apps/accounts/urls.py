bf4484525ea23920dc6daf3da09ba86d7a4ef16b
Gallibax <galymken@gmail.com>
2025-07-09 10:26:15 +0500

    celery, redis, кнопки авторизации

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import register, login_view
+from .views import register, login_view, check_auth_code, logout_view, profile
 
-app_name = 'accounts'
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_view , name='login'), 
+    path('check-auth-code/', check_auth_code, name='check_auth_code'),
+    path('logout/', logout_view, name='logout'),
+    path('profile/', profile, name='profile'),
 ]
--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include ('apps.accounts.urls', namespace='accounts')),
+    path('accounts/', include ('apps.accounts.urls')),
     path('', include('apps.products.urls')),  # подключение твоего index как главной страницы
 ]
 
